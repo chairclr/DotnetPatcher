48e31af07280fcda298ac4c1ff00e410249b94cb
Project ArkAngel <projectaa76@gmail.com>
2022-06-10 17:06:36 -0400

    fix worlds ugliest formatting

--- DotnetPatcher/Decompile/Decompiler.cs
+++ DotnetPatcher/Decompile/Decompiler.cs
@@ -24,7 +24,7 @@ namespace DotnetPatcher.Decompile
 	{
 		private readonly DecompilerSettings decompilerSettings = new DecompilerSettings(LanguageVersion.Latest)
 		{
-			CSharpFormattingOptions = FormattingOptionsFactory.CreateKRStyle()
+			CSharpFormattingOptions = FormattingOptionsFactory.CreateAllman()
 		};
 		private DecompilerUtility.ExtendedProjectDecompiler projectDecompiler;
 
